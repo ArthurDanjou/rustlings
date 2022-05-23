8e1c05f6e9a9a28a93449fdd236e53c5ea713a8e
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-05-23 18:37:38 +0200

    Working and learning with exercices

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -1,3 +1,5 @@
+use std::num::ParseIntError;
+
 // The From trait is used for value-to-value conversions.
 // If From is implemented correctly for a type, the Into trait should work conversely.
 // You can read more about it at https://doc.rust-lang.org/std/convert/trait.From.html
@@ -33,10 +35,28 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            Default::default()
+        } else {
+            let splits = s.splitn(2, ",").collect::<Vec<&str>>();
+            let name = splits[0];
+
+            if name.is_empty() {
+                Person::default()
+            } else if let Some(age) = splits.get(1) {
+                match age.parse() {
+                    Ok(age) => Person {
+                        name: String::from(name),
+                        age,
+                    },
+                    Err(_) => Person::default(),
+                }
+            } else {
+                Person::default()
+            }
+        }
     }
 }
 
